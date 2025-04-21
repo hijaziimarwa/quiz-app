b407af5298925ac66109ebe9b7fcdcd0ab133fa7
hijaziimarwa <hijaziimarwa@gmail.com>
2025-04-21 19:53:09 +0300

    Some edits

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,24 +1,17 @@
+document.querySelector('form').addEventListener('submit', function (e) {
+  e.preventDefault();
 
-  document.querySelector("form").addEventListener("submit", function (e) {
-    e.preventDefault();
-  
-    const email = document.querySelector('input[name="email"]').value;
-    const password = document.querySelector('input[name="password"]').value;
+  const email = document.querySelector('input[name="email"]').value;
+  const password = document.querySelector('input[name="password"]').value;
 
-    const storedEmail = localStorage.getItem("userEmail");
-    const storedPassword = localStorage.getItem("userPassword");
-  
-    
-    if (email === "admin@quiz.com" && password === "admin123") {
-      window.location.href = "../pages/dashboard.html";
-    }
-    
-    else if (email === storedEmail && password === storedPassword) {
-      window.location.href = "../pages/home.html";
-    }
-    
-    else {
-      alert("Email and password not found. Please try again !");
-    }
-  });
-  
+  const storedEmail = localStorage.getItem('userEmail');
+  const storedPassword = localStorage.getItem('userPassword');
+
+  if (email === 'admin@quiz.com' && password === 'admin123') {
+    window.location.href = '../pages/dashboard.html';
+  } else if (email === storedEmail && password === storedPassword) {
+    window.location.href = '../pages/home.html';
+  } else {
+    alert('Email and password not found. Please try again !');
+  }
+});
--- scripts/register.js
+++ scripts/register.js
@@ -1,18 +1,15 @@
+document.querySelector('form').addEventListener('submit', function (e) {
+  e.preventDefault();
 
-  document.querySelector("form").addEventListener("submit", function (e) {
-    e.preventDefault();
+  const username = document.querySelector('input[name="username"]').value;
+  const email = document.querySelector('input[name="email"]').value;
+  const password = document.querySelector('input[name="password"]').value;
 
-    const username = document.querySelector('input[name="username"]').value;
-    const email = document.querySelector('input[name="email"]').value;
-    const password = document.querySelector('input[name="password"]').value;
+  localStorage.setItem('userUsername', username);
+  localStorage.setItem('userEmail', email);
+  localStorage.setItem('userPassword', password);
 
-    localStorage.setItem("userUsername", username);
-    localStorage.setItem("userEmail", email);
-    localStorage.setItem("userPassword", password);
+  alert('Account created successfully!');
 
-
-    alert("Account created successfully!");
-
-    window.location.href = "../index.html";
-  });
-
+  window.location.href = '../index.html';
+});
