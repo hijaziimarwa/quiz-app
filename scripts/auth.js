eca057a1bbde69e766176fb5aea06b7f4ba75306
hijaziimarwa <hijaziimarwa@gmail.com>
2025-04-22 21:08:45 +0300

    Edit auth.js to provide many users

--- scripts/auth.js
+++ scripts/auth.js
@@ -4,14 +4,20 @@ document.querySelector('form').addEventListener('submit', function (e) {
   const email = document.querySelector('input[name="email"]').value;
   const password = document.querySelector('input[name="password"]').value;
 
-  const storedEmail = localStorage.getItem('userEmail');
-  const storedPassword = localStorage.getItem('userPassword');
+
+  const users = JSON.parse(localStorage.getItem('users')) || [];
+
+  const user = users.find(u => u.email === email && u.password === password);
+
 
   if (email === 'admin@quiz.com' && password === 'admin123') {
     window.location.href = '../pages/dashboard.html';
-  } else if (email === storedEmail && password === storedPassword) {
+  }
+
+  else if (user) {
     window.location.href = '../pages/home.html';
   } else {
-    alert('Email and password not found. Please try again !');
+    alert('Email and password not found. Please try again!');
   }
 });
+
