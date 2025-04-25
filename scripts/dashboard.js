f05aa8f76efcbba6da2f1e918a5fd751e6c7c176
hijaziimarwa <hijaziimarwa@gmail.com>
2025-04-25 23:11:58 +0300

    Functionality of dashboard page

---
+++ scripts/dashboard.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const isAdmin = localStorage.getItem('isAdmin');
+  
+   
+    const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+  
+    const user = users.find(u => u.email === loggedInUser.email);
+  
+    if (user) {
+      
+      const emailElement = document.getElementById('email');
+      const usernameElement = document.getElementById('name');
+      const scoreElement = document.getElementById('quiz-score');
+      const chosenQuizElement = document.getElementById('quiz-name');
+  
+      usernameElement.textContent = ` ${user.username}`;
+      emailElement.textContent = ` ${user.email}`;
+      scoreElement.textContent = `${user.score}`;
+      chosenQuizElement.textContent = ` ${user.chosenQuiz || 'Not attempted yet'}`;
+    }
+  });
+  
