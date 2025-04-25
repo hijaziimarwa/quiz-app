0f1a394b1036a62c6ea5f6335b780db0deb232c6
hijaziimarwa <hijaziimarwa@gmail.com>
2025-04-25 23:54:01 +0300

    Change path

--- scripts/auth.js
+++ scripts/auth.js
@@ -4,22 +4,16 @@ document.querySelector('form').addEventListener('submit', function (e) {
   const email = document.querySelector('input[name="email"]').value;
   const password = document.querySelector('input[name="password"]').value;
 
-
   const users = JSON.parse(localStorage.getItem('users')) || [];
 
-  const user = users.find(u => u.email === email && u.password === password);
-
+  const user = users.find((u) => u.email === email && u.password === password);
 
   if (email === 'admin@quiz.com' && password === 'admin123') {
-  
-    window.location.href = '../pages/dashboard.html';
-  }
-
-  else if (user) {
-  localStorage.setItem('loggedInUser', JSON.stringify(user));
-    window.location.href = '../pages/home.html';
+    window.location.href = '/pages/dashboard.html';
+  } else if (user) {
+    localStorage.setItem('loggedInUser', JSON.stringify(user));
+    window.location.href = '/pages/home.html';
   } else {
     alert('Email and password not found. Please try again!');
   }
 });
-
